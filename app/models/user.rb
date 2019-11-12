d861e7e27e85ce1849ab06c8d55ab8ed4b58f930
bosmanpa <bsmn.ptrck@gmail.com>
2019-11-12 11:56:13 -0600

    login/logout routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ has_many :owner_reservations, through: :cats, source: :reservations
 has_many :reservations, foreign_key: 'renter_id' #called this 'reservations' instead of 'renter_reservations'
 has_many :rented_cats, through: :reservations, source: :cat
 
+has_secure_password
 
   def top_3_cats #top 3 highest rated cats. If no cats, returns nil
    cats.length > 0 ? cats.sort_by{|cat| cat.overall_rating}.reverse[0..2] : nil
