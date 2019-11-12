ce87fcbf3e82cd752b6eae47af6827d13ad83732
Janae Hall <janaeelisa@gmail.com>
2019-11-12 12:56:23 -0600

    sessions controller and new view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,19 @@
 class SessionsController < ApplicationController
+  def new
+  end
 
+  def create
+    user = User.find_by(name: params[:name])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      redirect_to login_path
+    end
+  end
+
+  def delete
+    session[:user_id] = nil
+    redirect_to login_path
+  end
 end
