d5da0774ac2af3dc0cce3c37bcf32f9d5ae6253f
Janae Hall <janaeelisa@gmail.com>
2019-11-12 15:02:58 -0600

    added authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ has_many :reservations, foreign_key: 'renter_id' #called this 'reservations' ins
 has_many :rented_cats, through: :reservations, source: :cat
 
 has_secure_password
+validates :name, presence: true
+validates :username, presence: true, uniqueness: true
+validates :password, presence: true
+validates :password, confirmation: {case_sensitive: true}
 
   def top_3_cats #top 3 highest rated cats. If no cats, returns nil
    cats.length > 0 ? cats.sort_by{|cat| cat.overall_rating}.reverse[0..2] : nil
