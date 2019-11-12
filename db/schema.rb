0f82519a0ba1935c1a2666f36787124eae263a28
bosmanpa <bsmn.ptrck@gmail.com>
2019-11-12 13:11:31 -0600

    added new user form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_174200) do
+ActiveRecord::Schema.define(version: 2019_11_12_185849) do
 
   create_table "cat_tags", force: :cascade do |t|
     t.integer "cat_id"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2019_11_12_174200) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "username"
   end
 
 end
