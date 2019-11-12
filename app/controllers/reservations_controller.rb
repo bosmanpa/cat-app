26937dc545b4edcf4b88156b8e4a16d9a8984c5a
bosmanpa <bsmn.ptrck@gmail.com>
2019-11-12 11:39:45 -0600

    new reservation form

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -5,11 +5,20 @@ class ReservationsController < ApplicationController
     end
 
     def show
-        
+
     end
 
     def new
         @reservation = Reservation.new
+    end
+
+    def create
+        @reservation.create(reservation_params)
+    end
+
+    private
+    def reservation_params
+    params.require(:reservation).permit(:user_id, :cat_id, :date)
     end
 
 end
