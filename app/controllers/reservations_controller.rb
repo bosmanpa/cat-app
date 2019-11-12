7eaa2148432081db5bba59f0d0e5cf13c8af48ac
bosmanpa <bsmn.ptrck@gmail.com>
2019-11-12 11:20:32 -0600

    added views for reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,15 @@
 class ReservationsController < ApplicationController
+
+    def index
+        @reservations = Reservation.all
+    end
+
+    def show
+        
+    end
+
+    def new
+        @reservation = Reservation.new
+    end
+
 end
